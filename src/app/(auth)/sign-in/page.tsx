9c2b89a943f30de4318d408079d0ed3c83ddad00
Divyanshi-Varde <vardedinky1702@gmail.com>
2024-05-31 19:19:14 +0530

    Implemented shadcn in forms

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,30 +1,113 @@
 "use client";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import * as z from "zod";
+import { useToast } from "@/components/ui/use-toast";
+import { useRouter } from "next/navigation";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signInSchema } from "@/schema/signInSchema";
+import { signIn } from "next-auth/react";
 
-export default function Component() {
-  const { data: session } = useSession();
-  if (session) {
-    return (
-      <div>
-        Signed in as {session.user.email} <br />
-        <button
-          className=" bg-orange-400 rounded px-3 py-1 m-4 "
-          onClick={() => signOut()}
-        >
-          Sign out
-        </button>
-      </div>
-    );
-  }
+const SignInPage = () => {
+
+  const { toast } = useToast();
+  const router = useRouter();
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: "",
+      password: ""
+    },
+  });
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    const result=await signIn("credentials",{
+      redirect:false,
+      identifier:data.identifier,
+      password:data.password
+    })
+
+    if(result?.error){
+      toast({
+        title:"Login Failed",
+        description:"Incorrect username or password",
+        variant:"destructive"
+      })
+    }
+    if(result?.url){
+      router.replace("/dashboard")
+    }
+  };
   return (
-    <div>
-      Not signed in <br />
-      <button
-        className=" bg-orange-400 rounded px-3 py-1 m-4 "
-        onClick={() => signIn()}
-      >
-        Sign in
-      </button>
+    <div className="flex justify-center items-center min-h-screen bg-gray-200">
+      <div className="w-full max-w-md p-8 spaace-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-2xl font-extrabold tracking-tight lg:text-3xl mb-6">
+              Welcome Back 
+            to True Feedback
+          </h1>
+          <p className="mb-4"> Sign in to continue your secret conversations</p>
+        </div>
+
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              control={form.control}
+              name="identifier"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Email/Username</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Email/Username" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <FormField
+              control={form.control}
+              name="password"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Password</FormLabel>
+                  <FormControl>
+                    <Input placeholder="Password" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <Button type="submit">SignIn</Button>
+          </form>
+        </Form>
+        <div className="mt-4 text-center">
+          <p>
+            Not a Member Yet?
+            <Link
+              href={"/sign-up"}
+              className="text-blue-600 hover:text-blue-800"
+            >
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
-}
+};
+
+export default SignInPage;
